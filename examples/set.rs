d30e913ca4de1fd5ad01993f5d0ed4ed15467749
ouwenkg <2630582710@qq.com>
2020-08-26 21:30:18 +0800

    feat(test): add cache test

--- examples/set.rs
+++ examples/set.rs
@@ -2,10 +2,12 @@ use rust_cache::cache;
 use std::time::Duration;
 
 fn main() {
-    let mut cache = cache::new(100, 0);
+    let mut cache = cache::new(100, 5);
     cache.set("test".to_string(), 2, 100);
 
-    assert_eq!(cache.get("test".to_string()), 2);
-    std::thread::sleep(Duration::from_secs(200));
-    assert_eq!(cache.get("test".to_string()), 0);
+    println!("cache {:?}", cache);
+
+    assert_eq!(cache.get("test".to_string()).unwrap(), 2);
+    // std::thread::sleep(Duration::from_secs(200));
+    assert_eq!(cache.get("test".to_string()).unwrap(), 0);
 }
--- src/cache.rs
+++ src/cache.rs
@@ -4,7 +4,7 @@ use std::sync::{Arc, RwLock};
 use std::thread;
 use std::time::{Duration, SystemTime};
 
-const DEFAULT_EXPIRATION: u64 = 0;
+const DEFAULT_EXPIRATION: i64 = 0;
 
 #[derive(Debug)]
 pub struct Item {
@@ -19,49 +19,52 @@ pub struct RCache {
 
 #[derive(Debug)]
 pub struct Cache {
-    default_expiration: u64,
+    default_expiration: i64,
     items: HashMap<String, Item>,
     janitor: Janitor,
 }
 
 #[derive(Debug)]
 pub struct Janitor {
-    interval: u64,
+    interval: i64,
 }
 
-pub fn new(default_expiration: u64, clean_expiration: u64) -> RCache {
+pub fn new(default_expiration: i64, clean_expiration: i64) -> RCache {
     let items = HashMap::new();
     return new_cache_with_janitor(default_expiration, clean_expiration, items);
 }
 
 fn new_cache_with_janitor(
-    default_expiration: u64,
-    clean_expiration: u64,
+    default_expiration: i64,
+    clean_expiration: i64,
     items: HashMap<String, Item>,
 ) -> RCache {
     let c = new_cache(default_expiration, clean_expiration, items);
-    let c_clone = c.clone();
+    let mut c_clone = c.clone();
 
-    // if clean_expiration > 0 {
-    // start clean up janitor
-    // unimplemented!()
-    // let janitor = thread::spawn(move || {
-    //     // set time to clean
-    //     let ticker = tick(Duration::from_secs(clean_expiration));
-
-    //     loop {
-    //         ticker.recv().unwrap();
-    //         c_clone.delete_expired()
-    //     }
-    // });
-    // }
+    if clean_expiration > 0 {
+        // start clean up janitor
+        let _ = thread::spawn(move || {
+            // set time to clean
+            let ticker = tick(Duration::from_secs(clean_expiration as u64));
+            loop {
+                ticker.recv().unwrap();
+                println!("janitor doing clean work");
+                c_clone.delete_expired()
+            }
+        });
+    }
     c
 }
 pub fn new_cache(
-    default_expiration: u64,
-    clean_expiration: u64,
+    default_expiration: i64,
+    mut clean_expiration: i64,
     items: HashMap<String, Item>,
 ) -> RCache {
+    if clean_expiration == DEFAULT_EXPIRATION {
+        clean_expiration = -1;
+    }
+
     let c = Cache {
         default_expiration,
         items,
@@ -75,12 +78,29 @@ pub fn new_cache(
     };
 }
 
+impl Item {
+    pub fn is_expired(&self) -> bool {
+        let now = SystemTime::now();
+
+        if let Ok(passed) = now.duration_since(self.expiration) {
+            if passed > Duration::from_secs(0) {
+                return true;
+            }
+        }
+        false
+    }
+}
+
 impl RCache {
-    pub fn set(&mut self, key: String, value: u64, ed: u64) {
+    pub fn set(&mut self, key: String, value: u64, mut ed: i64) {
         let c_lock = self.cache.clone();
         let mut c = c_lock.write().unwrap();
 
-        let expiration_time = SystemTime::now().checked_add(Duration::from_secs(ed));
+        if ed == DEFAULT_EXPIRATION {
+            ed = c.default_expiration
+        }
+
+        let expiration_time = SystemTime::now().checked_add(Duration::from_secs(ed as u64));
         let i = Item {
             object: value,
             expiration: expiration_time.unwrap(),
@@ -99,17 +119,30 @@ impl RCache {
         c.items.clear()
     }
 
-    pub fn get(&self, key: String) -> u64 {
+    pub fn delete_expired(&mut self) {
+        let c_lock = self.cache.clone();
+        let mut cw = c_lock.write().unwrap();
+
+        let cr = c_lock.write().unwrap();
+        for (key, item) in cr.items.iter() {
+            if item.is_expired() {
+                println!("janitor cleaned key {:?}", key);
+                let _ = cw.items.remove_entry(key);
+            }
+        }
+    }
+
+    pub fn get(&self, key: String) -> Option<u64> {
         let c_lock = self.cache.clone();
         let c = c_lock.read().unwrap();
 
         if let Some(i) = c.items.get(&key) {
             if SystemTime::now() > i.expiration {
-                return 0;
+                return Some(0);
             }
-            return i.object;
+            return Some(i.object);
         }
-        0
+        Some(0)
     }
 
     pub fn item_count(&self) -> usize {
@@ -118,26 +151,28 @@ impl RCache {
 
         c.items.len()
     }
-
-    pub fn delete_expired(&self) {
-        unimplemented!()
-        // for key, item in self.items() {
-        //     if item.expiration > 0 && now > item.expiration {
-        //         self.item.delete(key)
-        //     }
-        // }
-    }
 }
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn test_set_u64() {
-        unimplemented!();
-    }
+    use super::*;
 
     #[test]
-    fn test_set_default() {
-        unimplemented!();
+    fn test_cache() {
+        let mut tc = new(DEFAULT_EXPIRATION, 0);
+
+        assert_eq!(tc.get("a".to_string()).unwrap(), 0);
+        assert_eq!(tc.get("b".to_string()).unwrap(), 0);
+        assert_eq!(tc.get("C".to_string()).unwrap(), 0);
+
+        tc.set("a".to_string(), 1, DEFAULT_EXPIRATION);
+        tc.set("b".to_string(), 2, DEFAULT_EXPIRATION);
+        tc.set("C".to_string(), 3, DEFAULT_EXPIRATION);
+
+        println!("cache is {:?}", tc);
+
+        assert_eq!(tc.get("a".to_string()).unwrap(), 1);
+        assert_eq!(tc.get("b".to_string()).unwrap(), 2);
+        assert_eq!(tc.get("C".to_string()).unwrap(), 3);
     }
 }
